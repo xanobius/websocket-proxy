b3bded8714847e9d24f495400d6df35064820e3c
Philipp Lauwiner <philipp.lauwiner@treestones.ch>
2020-09-18 21:56:52 +0200

    finished basic integration

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require("http")
 const WebSocketServer = require("websocket").server
 const mysql = require("mysql")
 
-let ws_connection = null;
+let ws_clients = [];
+let ws_dashies = [];
+let users = [];
 
 let db_connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -25,9 +27,8 @@ db_connection.connect((err) => {
 db_connection.query("SELECT id, api_token FROM users", (err, rows) => {
   if(err) throw err;
   console.log(rows);
+  users = rows;
 })
-
-
 
 const httpserver = http.createServer((req, res) => {
   console.log("we have received a request");
@@ -41,25 +42,78 @@ websocket.on("request", request => {
   let params = new URLSearchParams(String(request.httpRequest.url).slice(2));
 
   if (request.origin == process.env.DASH_ORIGIN) {
-    console.log("Origin" + request.origin);
-    // just require user id
-    console.log('dashboard request : ' + params.get('token'));
-  }else{
-    // require token
-    console.log('external request: ' + params.get('token'));
-  }
+    // Request from dashboard
+    if(users.filter(u => u.id == params.get('token')).length === 1){
+      console.log('user legit, register dashy')
+      let dashy = bindDash({
+        'id' : params.get('token'),
+        'connection' : request.accept(null, request.origin)
+      })
 
-  ws_connection = request.accept(null, request.origin)
-  ws_connection.on("open", (e, req) => {
+      if (ws_dashies.filter(d => d.id == params.get('token')).length){
+        // existing connection, override
+        ws_dashies.filter(d => d.id == params.get('token'))[0] = dashy
+      }else{
+        // register new item for this user
+        ws_dashies.push(dashy)
+      }
+    }else{
+      request.reject('Wrong token - Invalid user id');
+    }
+  }else{
+    // Request from student clients
+    console.log('external request: ' + params.get('token'));
+    if(users.filter(u => u.api_token == params.get('token')).length === 1) {
+      let usr = users.filter(u => u.api_token == params.get('token'))[0];
+
+      let newCon = {
+        'userId' : usr.id,
+        'connection' : request.accept(null, request.origin)
+      }
+      ws_clients.push(bindClient(newCon))
+      newCon.connection.send('Welcome, take a seat!')
+    }else{
+      request.reject('Wrong token');
+    }
+
+  }
+})
+
+
+function bindDash(dash)
+{
+  dash.connection.on("open", (e, req) => {
     console.log('OPENED')
     console.log(e);
     console.log(`Conn URL ${req.url}`);
   })
-  ws_connection.on("close", e => console.log("Closed"))
-  ws_connection.on("message", e => {
+  dash.connection.on("close", e => console.log("Closed"))
+  dash.connection.on("message", e => {
     console.log(`Received message ${e.utf8Data}`)
   })
-})
+  return dash;
+}
 
+function bindClient(usrItem)
+{
+  usrItem.connection.on("open", (e, req) => {
+    console.log('OPENED')
+    console.log(e);
+    console.log(`Conn URL ${req.url}`);
+  })
+  usrItem.connection.on("close", e => console.log("Closed"))
+  usrItem.connection.on("message", e => {
+    console.log(`Received message ${e.utf8Data}`)
+
+    ws_dashies
+      .filter(d => d.id == usrItem.userId)
+      .forEach(d => {
+        d.connection.send(e.utf8Data)
+      })
+
+  })
+  return usrItem;
+}
 
 httpserver.listen(8980, () => console.log("Server is listening on port 8980"));
+
